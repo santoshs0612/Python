23876cec12e9e79815d55c17348b26d01533543e
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-25 05:49:08 +0000

    string initials

---
+++ Basics/05_string.py
@@ -0,0 +1,22 @@
+# string 
+name = "Santosh"
+friend = "Murali"
+anotherFriend = "Sudhunshau"
+
+apple = """He said
+i wanto to meet 
+where?
+Delhi?
+no
+why?
+
+ """
+
+# print("Hello, ",  name)
+# print(apple)
+
+# String is like aray of char 
+# print(name[0])
+
+for c in name:
+    print(c)
