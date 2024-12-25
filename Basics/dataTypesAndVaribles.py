4b904df99936d6c7308a2ffdf2ad513776e314a1
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-25 04:52:14 +0000

    dataTypes and Variables

---
+++ Basics/dataTypesAndVaribles.py
@@ -0,0 +1,25 @@
+a =1000000
+print(a)
+print("The Type of a is: ", type(a))
+
+b= "Santosh"
+print(b)
+print( "The Type of b is: " ,type(b))
+
+c = complex(8,2)
+print(c)
+
+# Boolean 
+d = True,
+
+list1 = [8,3,"Hello"]
+print(list1)
+
+tuple1 ={8,3,"Hello"}
+print(tuple1)
+
+# Map Data types (Dictinary)
+dict1 = {"name": "Santosh", "age":40,"CanVote": True}
+print(dict1)
+
+print(type(dict1))
