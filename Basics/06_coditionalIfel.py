d7d4402881c0688f9a861a931060aff9040a3ff5
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-25 07:01:00 +0000

    if else elif nested

---
+++ Basics/06_coditionalIfel.py
@@ -0,0 +1,41 @@
+
+a  = int(input("Enter your age: "))
+
+print("Your Age is:",a)
+
+if(a >18):
+    print("You can drive")
+else:
+    print("You can not drive")
+
+# Conditionals 
+# > < >= <=, ==,!=
+
+
+#  elif
+num = int(input("Enter the value of num: "))
+if (num < 0):
+  print("Number is negative.")
+elif (num == 0):
+  print("Number is Zero.")
+elif (num == 999):
+  print("Number is Special.")
+else:
+  print("Number is positive.")
+
+print("I am happy now")
+
+
+#  Nested if else
+num = 18
+if (num < 0):
+    print("Number is negative.")
+elif (num > 0):
+    if (num <= 10):
+        print("Number is between 1-10")
+    elif (num > 10 and num <= 20):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is zero")
