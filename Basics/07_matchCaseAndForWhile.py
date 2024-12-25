2479877baa386ad32877805d139a0637b01d9b2e
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-25 09:30:59 +0000

    while loop

--- Basics/07_matchCaseAndForWhile.py
+++ Basics/07_matchCaseAndForWhile.py
@@ -1,16 +1,16 @@
 
 # x = int(input("Enter the value of X: "))
 
-match x:
+# match x:
 
-    case 0:
-        print("X is Zero")
-    case 1:
-        print("X is one")
-    case _ if x!=3:
-        print("It is Not three")
-    case _:
-        print("Anoter Number")
+#     case 0:
+#         print("X is Zero")
+#     case 1:
+#         print("X is one")
+#     case _ if x!=3:
+#         print("It is Not three")
+#     case _:
+#         print("Anoter Number")
 
 #  ============ For Loops +++++++++++++++++++
 
@@ -33,5 +33,24 @@ match x:
 #   print(k)
 
   
-for k in range(1, 12, 3):
-  print(k)
+# for k in range(1, 12, 3):
+#   print(k)
+
+
+i =0
+while i <= 3:
+    # print(i)
+    i= i+1
+i = int(input("Enter the number: "))
+print(i)
+while(i<=38):
+  i = int(input("Enter the number: "))
+  print(i)
+
+print("Done with the loop")
+
+count=5
+while(count>0):
+   print(count)
+   count=count-1
+
