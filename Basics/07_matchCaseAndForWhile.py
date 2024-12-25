1969a67aa4a568f48d3e1fb9e64192cf7344e657
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-25 09:23:39 +0000

    For loops

--- Basics/07_matchCaseAndForWhile.py
+++ Basics/07_matchCaseAndForWhile.py
@@ -1,5 +1,5 @@
 
-x = int(input("Enter the value of X: "))
+# x = int(input("Enter the value of X: "))
 
 match x:
 
@@ -7,5 +7,31 @@ match x:
         print("X is Zero")
     case 1:
         print("X is one")
+    case _ if x!=3:
+        print("It is Not three")
     case _:
         print("Anoter Number")
+
+#  ============ For Loops +++++++++++++++++++
+
+# name = 'Abhishek'
+# for i in name:
+#   print(i)
+#   if(i =="b"):
+#     print("This is something special!")
+    
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#   print(color)
+#   for i in color:
+#     print(i)
+
+# for k in range(5):
+#   print(k + 1)
+  
+# for k in range(1, 20001):
+#   print(k)
+
+  
+for k in range(1, 12, 3):
+  print(k)
