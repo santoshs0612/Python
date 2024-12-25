5f86ea5dc9f47cfc62e01b9fec48e78484814429
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-25 05:41:34 +0000

    type casting and inputs

---
+++ Basics/04_TypeCasting.py
@@ -0,0 +1,29 @@
+
+#TypeCasting
+
+
+a = "1"
+# a = 1
+b= "2"
+# b= 2
+# Explicit TyeCasting 
+# print((int)(a)+(int)(b))
+
+c= 1.9
+d =8
+# Implicit TypeCasting
+# print(c+d)
+
+# Inputs Taking user Input 
+
+a  =  input("Enter your Name: ")
+print("My Name is ", a)
+
+# input gives a str calss to use it as int use type csting 
+
+x = input("Enter First Num1 ")
+y = input("Enter second Num1 ")
+
+print( x +y)
+print(int(x)+int(y))
+
