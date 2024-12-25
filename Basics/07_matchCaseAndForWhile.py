4ad726a4f0aca8483f2960e8532801cec3caab16
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-25 07:24:59 +0000

    switch case as match

---
+++ Basics/07_matchCaseAndForWhile.py
@@ -0,0 +1,11 @@
+
+x = int(input("Enter the value of X: "))
+
+match x:
+
+    case 0:
+        print("X is Zero")
+    case 1:
+        print("X is one")
+    case _:
+        print("Anoter Number")
