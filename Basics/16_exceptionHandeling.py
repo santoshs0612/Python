1c356546a4f62608598dfca371bcf8146c8e50c2
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-25 14:29:43 +0000

    exception handeling and raing custom error and finally

--- Basics/16_exceptionHandeling.py
+++ Basics/16_exceptionHandeling.py
@@ -10,11 +10,45 @@
 # print("End of program")
 
 
-try:
-    num = int(input("Enter an integer: "))
-    a = [6, 3]
-    print(a[num])
-except ValueError:
-    print("Number entered is not an integer.")
-except IndexError:
-    print("Index out of bound")
+# try:
+#     num = int(input("Enter an integer: "))
+#     a = [6, 3]
+#     print(a[num])
+# except ValueError:
+#     print("Number entered is not an integer.")
+# except IndexError:
+#     print("Index out of bound")
+
+# ========== Finally Clause ===========>
+
+#  must executed code part 
+
+# try:
+#     l = [1,5,6,8]
+#     i =int(input("Enter the Index: "))
+#     print(l[i])
+# except:
+#     print("some eror occured ")
+# # finally:
+# #     print("Im always executed")
+
+def func1():
+    try:
+        l = [1,5,6,8]
+        i =int(input("Enter the Index: "))
+        print(l[i])
+    except:
+        print("some eror occured ")
+    finally:
+        print("Im always executed")
+
+# x = func1()
+# print(x)
+
+#   ====== Raising Custom error  ======>
+
+a = int(input("ENter any vlaue between 5 and 9 "))
+
+if(a< 5 or a>9):
+    raise ValueError("Value should be between 5 and 9")
+
