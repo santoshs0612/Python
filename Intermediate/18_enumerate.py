f8e971a81459f2ca8b22f6a53888c858ba9fc620
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-26 06:02:58 +0000

    enumerate in for loop

---
+++ Intermediate/18_enumerate.py
@@ -0,0 +1,21 @@
+marks = [12,56,32,98,12,45,1,4]
+# index =0
+# for mark in marks:
+#     print(mark)
+#     if index ==3:
+#         print("Santosh Awsome")
+#     index =index+1
+
+
+# for index,mark in enumerate(marks):
+#     print(mark)
+#     if index ==3:
+#         print("Santosh Awsome")
+
+
+for index,mark in enumerate(marks,start =1):
+    print(mark)
+    if index ==3:
+        print("Santosh Awsome")
+
+
