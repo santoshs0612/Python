c18c89e1607c1e1915a6b8fe9233ddf82d9e840c
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-26 10:07:22 +0000

    is and equal difference

---
+++ Intermediate/28_equalAndis.py
@@ -0,0 +1,19 @@
+# a = 4
+# b = "4"
+
+# a =[1,2,3]
+# b =[1,2,3]
+
+# a =3
+# b=3
+
+# a = (1,2,4)
+# b =(1,2,4)
+# is works for non immutable objects 
+
+a =None
+b =None
+
+print(a is b) # exact locaiton of objext memory
+
+print(a==b) # value 
