dfd0904f5f0885105da8d2fd62b67d73d08356e3
santoshs0612 <santoshs0612@gmail.com>
2024-12-27 14:25:06 +0530

    Dunder Metods explanation

---
+++ Oops/13_MagicAnd Dunder.py
@@ -0,0 +1,21 @@
+# Magic and Dunder Methods 
+
+from emp import Employee
+
+# class Employee:
+
+#     name = "Santosh"
+#     def __init__(self,name):
+#         self.name=name
+#     def __len__(self):
+#         i=0
+#         for c in self.name:
+#             i=i+1
+#         return i
+
+
+e = Employee("Snatsh")
+print(e)
+# print(e.name)
+# print(len(e))
+
