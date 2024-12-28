22c230349f4f6545b221d4337a7ea1ea8f10e719
santoshs0612 <santoshs0612@gmail.com>
2024-12-28 10:24:35 +0530

    Single inheritance concept

---
+++ Oops/16_singleInhertance.py
@@ -0,0 +1,30 @@
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        
+    def make_sound(self):
+        print("Sound made by the animal")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Dog")
+        self.breed = breed
+        
+    def make_sound(self):
+        print("Bark!")
+
+class Cat(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Cat")
+        self.breed = breed
+        
+    def make_sound(self):
+        print("myiuuuu!")
+d = Dog("Dog", "Doggerman")
+d.make_sound()
+
+a = Animal("Dog", "Dog")
+a.make_sound()
+
+# Quick Quiz: Implement a Cat class by using the animal class. Add some methods specific to cat
