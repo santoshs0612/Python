e5aa4cd0e670e692e01050e5e8789fe6d34af260
santoshs0612 <santoshs0612@gmail.com>
2024-12-29 18:49:04 +0530

    Request module in python

--- Advance/05_requestModule.py
+++ Advance/05_requestModule.py
@@ -1,4 +1,30 @@
 import requests
 
-response = requests.get("https://www.google.com")
-print(response.text())
+# response = requests.get("https://www.google.com")
+# print(response.text)
+
+# url = "https://jsonplaceholder.typicode.com/posts"
+
+# data = {
+#     "title": 'harry',
+#     "body": 'bhai',
+#     "userId": 12,
+#   }
+# headers =  {
+#     'Content-type': 'application/json; charset=UTF-8',
+#   }
+# response = requests.post(url, headers=headers, json=data)
+
+# print(response.text)
+
+
+from bs4 import BeautifulSoup
+url = "https://www.codewithharry.com/blogpost/django-cheatsheet/"
+r = requests.get(url)
+# print(r.text)
+
+
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup.prettify())
+for heading in soup.find_all("h2"):
+  print(heading.text)
